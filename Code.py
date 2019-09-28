f41bcf226ea13147d49591beb59f512ed27ac29c
Basu Jindal <42815171+martianrainbow@users.noreply.github.com>
2019-09-28 23:29:46 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+user = "   "
+password = "  "
+
+driver = webdriver.Chrome(r"C:\Users\Basu Jindal\chromedriver.exe")
+driver.set_page_load_timeout(10)
+
+driver.get("https://netaccess.iitm.ac.in/account/login/")
+
+elem = driver.find_element_by_id("username")
+elem.send_keys(user)
+
+elem = driver.find_element_by_id("password")
+elem.send_keys(password)
+
+elem1 = driver.find_element_by_name("submit")
+elem1.send_keys(Keys.RETURN)
+
+driver.get("https://netaccess.iitm.ac.in/account/approve")
+
+day1 = driver.find_element_by_id("radios-1").click()
+
+approve = driver.find_element_by_name("approveBtn").click()
+
+driver.quit()
+print('task completed')
